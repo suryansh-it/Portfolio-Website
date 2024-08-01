daf47aacccb8bd70f7267ae2c0b33ba78cfc5484
Suryansh Sharma <suryansharma09@gmail.com>
2024-08-01 14:41:10 +0530

    modifying backedn code

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 from flask import Flask, render_template, request, jsonify, url_for
 from models import db , Visitor
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'your_secret_key'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@localhost/dbname'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+
+    with app.app_context():
+        db.create_all()  # Create tables based on the model definitions
+
+    return app
+
+app= create_app()
 
 @app.route('/')
 def index():
--- models.py
+++ models.py
@@ -10,10 +10,10 @@ class Prof_data(db.Model):
     name = db.Column(db.String(80),   nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     subject = db.Column(db.String(80),  nullable=False)
-    message = db.Column(db.String(80),  nullable=False)
-    profile_data = db.Column(JSON)  # JSON column for storing arbitrary JSON data
+    message = db.Column(db.String(500),  nullable=False)
+    # profile_data = db.Column(JSON)  # JSON column for storing arbitrary JSON data
 
 
 
-    def __repr__(self):
-        return f'<Prof_data {self.name}>'
+    # def __repr__(self):
+    #     return f'<Prof_data {self.name}>'
