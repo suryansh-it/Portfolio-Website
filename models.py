e60ef80f81d34f35d057fafc4e741bbc88d084b2
Suryansh Sharma <suryansharma09@gmail.com>
2024-08-04 21:37:05 +0530

    added admin data model

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.dialects.postgresql import JSON
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
@@ -42,3 +43,17 @@ class CommentData(db.Model):
     author = db.Column(db.String(80),  nullable=False)
     date_created = db.Column(db.DateTime, default=db.func.now())
     blog_id = db.Column(db.Integer, db.ForeignKey('Blog_Data.id'), nullable= False) #blog_id fk references id in blog_data
+
+
+class AdminData(db.Model):
+    __tablename__ = 'Admin_Data'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
