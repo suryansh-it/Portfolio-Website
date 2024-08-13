ae7f4c83e2bb14a81203e205099344e33c1d5908
Suryansh Sharma <suryansharma09@gmail.com>
2024-08-13 17:07:38 +0530

    delete project functionality added successfully

--- static/js/delete_project.js
+++ static/js/delete_project.js
@@ -1,13 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Select all buttons with the class "delete-button" and attach an event listener to each
-    document.querySelectorAll(".delete-button").forEach(button => {
+    const deleteButtons = document.querySelectorAll(".delete-project");
+
+    deleteButtons.forEach(button => {
         button.addEventListener("click", function(event) {
-            // Prevent the default form submission
             event.preventDefault();
-
-            // Retrieve the post ID from the data-post-id attribute of the clicked button
-            let projectId = this.dataset.postId;
-
+            const projectId = this.getAttribute("data-project-id");
             // Confirm with the user if they really want to delete the post
             if (confirm("Are you sure you want to delete this blog post?")) {
                 // Send a DELETE request to the server to delete the post with the specified ID
@@ -17,14 +14,20 @@ document.addEventListener("DOMContentLoaded", function() {
                         "Content-Type": "application/json" // Specify the content type as JSON
                     }
                 })
-                .then(response => response.json()) // Parse the response as JSON
-                .then(data => {
+                .then(response => {
+                    if (response.ok) {
+                        alert("Project deleted successfully.");
+                        location.reload(); // Reload the page to update the project list
+                    } else {
+                        alert("Failed to delete the project.");
+                    }}) // Parse the response as JSON
+                
                     // Alert the user with the response message from the server
                     alert(data.message);
 
                     // Redirect the user to the view_blog page to see the updated list of posts
                     window.location.href = "/view_project";
-                })
+                
                 
             }
         });
